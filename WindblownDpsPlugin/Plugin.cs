735c6fbb35608f683f174d4740b66e9a5d227032
silso <chroelo@gmail.com>
2025-01-04 22:46:31 -0600

    Switch to il2cpp template

--- WindblownDpsPlugin/Plugin.cs
+++ WindblownDpsPlugin/Plugin.cs
@@ -1,17 +1,18 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
 
 namespace WindblownDpsPlugin;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-public class Plugin : BaseUnityPlugin
+public class Plugin : BasePlugin
 {
-    internal static new ManualLogSource Logger;
-        
-    private void Awake()
+    internal static new ManualLogSource Log;
+
+    public override void Load()
     {
         // Plugin startup logic
-        Logger = base.Logger;
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Log = base.Log;
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
     }
 }
---
+++ WindblownDpsPlugin/obj/Debug/net6.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace WindblownDpsPlugin
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "WindblownDpsPlugin";
+        public const string PLUGIN_NAME = "My first plugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
